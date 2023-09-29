ef84d51788d6f74190c996156d5ff1ea65006ca7
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-09-29 23:51:35 +0530

    Add files via upload

---
+++ Alpha Batch/Array/BinarySearch.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class BinarySearch {
+ 
+    public static int binarySearch(int numbers[] , int key){
+        int start = 0;
+        int end = numbers.length-1;
+
+        while (start <= end) {
+            int mid = (start+end)/2;
+
+            //comparisons
+            if (numbers[mid] == key) {
+                return mid;
+            }
+            if (numbers[mid] >= key) { //left case
+                end = mid-1;
+            }
+            else{                     //right case
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+
+public static void main(String[] args) {
+    int numbers[] = {2 , 4 , 6 , 8 , 10 , 12 , 14};
+    int key = 10;
+    System.out.println("Index for key is : " + binarySearch(numbers, key));
+}    
+}
