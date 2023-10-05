bf95727cd2e4a7b3a22c2ad4c9639cd9b3bf380b
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-10-05 23:55:23 +0530

    Add files via upload

---
+++ Alpha Batch/Sorting Techniques/InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+
+    public static void iSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            // Finding out the current position to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            // insertion
+            arr[prev + 1] = curr;
+        }
+    }
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5 , 4 , 1 , 3,2};
+        iSort(arr);
+        printArr(arr);
+     }
+}
+//TC-> O(n^2)
