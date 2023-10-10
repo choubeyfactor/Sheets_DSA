f5ba8568e4de9e78ac8dd6f5a1ff645273e1f373
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-10-10 23:50:27 +0530

    Add files via upload

---
+++ Alpha Batch/String/Routing.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Routing {
+    
+    public static float getShortestPath(String path ){
+        int x =0 , y=0;
+
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            //south
+            if (dir == 'S') {
+                y--;
+            }
+            //north
+            else if (dir == 'N') {
+                y++;
+            }
+            //east
+            else if (dir == 'W') {
+                x--;
+            }
+            else{
+                x++;
+            }
+
+            int x2=x*x;
+            int y2 = y*y;
+            return Math.sqrt(x2+y2);
+        }
+    }
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
+    }
+}
+//TC->O(n)
