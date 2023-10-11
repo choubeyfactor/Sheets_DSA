9a4f00571f56493af64cea34572bdb159653d49e
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-10-11 23:54:16 +0530

    Add files via upload

---
+++ Alpha Batch/String/ValidAnagram.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s , String t){
+//converting the character string into array of characters
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+//sorting the elements of character array in alphabetical order
+        Arrays.sort(sChar);
+        Arrays.sort(tChar);
+//comparing the sorted character array and deriving results
+        return Arrays.equals(sChar , tChar);
+    } 
+}
