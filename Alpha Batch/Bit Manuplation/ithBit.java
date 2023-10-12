5c8e3752f3d49f2bf58e62d33c06e98eadb7361b
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-10-12 23:27:49 +0530

    Add files via upload

---
+++ Alpha Batch/Bit Manuplation/ithBit.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ithBit {
+   
+ public static int getIthBit(int n , int i){
+//basically in this in agiven number when converted to binary , we want to fetch ith position of that
+    int bitMask = 1<<i;
+
+    if ((n & bitMask) == 0) {
+        return 0;
+    }else{
+        return 1;
+    }
+ }
+
+ public static int setIthBit(int n , int i ){
+    int bitMask = 1<<i;
+    return n | bitMask;
+ }
+
+ public static int clearIthBit(int n , int i){
+    int bitMask = ~(1<<i);
+    return n & bitMask;
+ }
+    public static void main(String[] args) {
+        System.out.println(getIthBit(10, 3));
+        System.out.println(setIthBit(10, 2));
+        System.out.println(clearIthBit(10, 1));
+
+    }
+}
