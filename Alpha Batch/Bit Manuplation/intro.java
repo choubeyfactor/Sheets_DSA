e870e7d4a053b227ceebec1e4d59fafee97961a0
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-10-12 23:27:33 +0530

    Add files via upload

---
+++ Alpha Batch/Bit Manuplation/intro.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class intro {
+
+    public static void main(String[] args) {
+        System.out.println(5^6); //xor -> different bits = 1 otherwise 0
+        System.out.println(5&6);//and
+        System.out.println(5|6);//or
+        System.out.println(~5); //not
+
+/*          Binary Left Shift: a<<b = (a)*(2^b)              */
+System.out.println(5<<2);
+/*          Binary Right Shift:a>>b = (a)/(2^b)              */ 
+
+
+}
+}
