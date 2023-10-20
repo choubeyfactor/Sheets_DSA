20730e3c7fabb33aa2103b1d0e8010880301edb8
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-10-20 23:28:38 +0530

    Add files via upload

---
+++ Sheet 2 Striver/Strings/ValidAnagram.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+    }
+    
+}
+
