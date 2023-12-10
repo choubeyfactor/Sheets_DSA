e388374f7f3718561735a91f657a8bbf59b29302
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-12-10 23:03:03 +0530

    Add files via upload

---
+++ Alpha Batch/LinkedList/llInJavaCollectionFramework.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class llInJavaCollectionFramework {
+    public static void main(String[] args) {
+        //create: pbjects int , float , boolean -> Integer , Float , Character
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        //add
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        //0->1->2
+        System.out.println(ll);
+        //remove
+        ll.removeFirst();
+        ll.removeLast();
+        System.out.println(ll);
+    }
+}
